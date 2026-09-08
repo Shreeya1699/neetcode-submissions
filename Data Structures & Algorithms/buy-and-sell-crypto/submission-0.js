1d512ff0e8b3673d498694e8446695ef55a402f0
Shreeya Nelekar <40059938+Shreeya1699@users.noreply.github.com>
2026-09-08 11:52:07 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = prices[0];
+    let result = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        result = Math.max(result, prices[i] - minPrice);
+        minPrice = Math.min(minPrice, prices[i]);
+    }
+
+    return result;
+    }
+}
